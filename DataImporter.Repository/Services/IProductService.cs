2bb9b1c99d4fd073af75368dfb21a90e5ebacf48
Ahmed Eissa <eissamail@googlemail.com>
2021-05-23 17:06:52 +0100

    Added DataImporter.Repository.Services

---
+++ DataImporter.Repository/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace DataImporter.Repository.Services
+{
+    public interface IProductService
+    {
+        Task SaveAsync(Product product);
+        Task GetByIdAsync(int id);
+        Task GetAllAsync();
+    }
+}
---
+++ DataImporter.Repository/Services/ProductService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataImporter.Repository.Services
+{
+    public class ProductService:IProductService 
+    {
+    }
+}
